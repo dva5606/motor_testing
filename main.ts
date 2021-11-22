877da39c00b5277e2e0dcca46cf22828ed94ebd1
dva5606 <86691169+dva5606@users.noreply.github.com>
2021-11-23 11:35:03 +1300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P14, 180)
+    pins.servoWritePin(AnalogPin.P14, 0)
 })
 input.onButtonPressed(Button.AB, function () {
-    servos.P0.setAngle(90)
-    pins.servoWritePin(AnalogPin.P14, 90)
+    pins.servoWritePin(AnalogPin.P0, 180)
+    pins.servoWritePin(AnalogPin.P14, 180)
 })
 input.onButtonPressed(Button.B, function () {
-    servos.P0.setAngle(180)
+    pins.servoWritePin(AnalogPin.P0, 0)
+    servos.P0.stop()
 })
 basic.showIcon(IconNames.House)
 pins.digitalWritePin(DigitalPin.P0, 1)
